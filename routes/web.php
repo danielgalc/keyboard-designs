62994da1c8a11442eb6b3958557afc46d1ef61eb
danielgalc <r2kads@gmail.com>
2026-04-26 19:28:22 +0200

    Add design editing and remove duplicate verification history
    - DesignController: edit() and update() methods (file replacement optional)
    - Route: GET /designs/{design}/edit and PATCH /designs/{design}
    - Designs/Edit.jsx: same UX as Create with data pre-filled, shows current file
    - Show.jsx: Editar button in header linking to edit page
    - Show.jsx: remove redundant verification history table (covered by traceability)

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/designs/create',                             [DesignController::class, 'create'])->name('designs.create');
     Route::post('/designs',                                   [DesignController::class, 'store'])->name('designs.store');
     Route::get('/designs/{design}',                           [DesignController::class, 'show'])->name('designs.show');
+    Route::get('/designs/{design}/edit',          [DesignController::class, 'edit'])->name('designs.edit');
+    Route::patch('/designs/{design}',             [DesignController::class, 'update'])->name('designs.update');
     Route::get('/designs/{design}/download',                  [DesignController::class, 'download'])->name('designs.download');
     Route::delete('/designs/{design}',                        [DesignController::class, 'destroy'])->name('designs.destroy');
     Route::post('/designs/{design}/settings/{printer}',       [PrinterSettingController::class, 'upsert'])->name('designs.settings.upsert');
